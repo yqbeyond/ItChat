a107a687298dd74d1831ff9378f6ed3ac81b0eac
Qi Yuan <yqqbeyond@gmail.com>
2017-02-23 19:58:35 +0800

    Update config.py

--- itchat/config.py
+++ itchat/config.py
@@ -4,6 +4,6 @@ VERSION = '1.2.27'
 BASE_URL = 'https://login.weixin.qq.com'
 OS = platform.system() #Windows, Linux, Darwin
 DIR = os.getcwd()
-DEFAULT_QR = 'QR.jpg'
+DEFAULT_QR = 'QR.png'
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
